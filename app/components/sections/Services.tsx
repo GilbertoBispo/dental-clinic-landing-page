af9362c4eb66592868c06bd32f05d4f196f83a44
Gilberto <gil79430@gmail.com>
2026-08-14 19:48:55 -0300

    feat(services): trava a largura dos cards da seção 'Our Services' em 480px

--- app/components/sections/Services.tsx
+++ app/components/sections/Services.tsx
@@ -48,7 +48,7 @@ export default function Services() {
                 <p className="font-semibold">High-Quality Care for Your Smile</p>
             </div>
 
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 w-full justify-items-center gap-y-5">
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 w-full justify-items-center gap-y-5 2xl:w-fit 2xl:h-fit 2xl:gap-5">
                 {/* utiliza o método .map() para renderizar cada card */}
                 {SERVICES.map((service, index) => {
                     // cada atributo do objeto SERVICES vira uma constante
@@ -65,7 +65,7 @@ export default function Services() {
                             whileInView={{ opacity: 1, y: 0}}
                             viewport={{once: true, margin: "-15px"}}
                             transition={{duration: 0.4, delay: 0.3}}>
-                            <Cards className="flex flex-col p-5 gap-5 text-shadow-lg w-[90%] md:w-[45vw] md:h-full lg:w-[31vw]">
+                            <Cards className="flex flex-col p-5 gap-5 text-shadow-lg w-[90%] md:w-[45vw] md:h-full lg:w-[31vw] 2xl:w-120">
                                 <div className="flex justify-between items-center">
                                     <img className="w-20" src={`${icon}.svg`} alt="sparkles icon"/>
                                     <h3 className="flex-1 text-center flex flex-col justify-center text-2xl h-20"><strong>{title}</strong></h3>
