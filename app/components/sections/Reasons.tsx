ff912f2a7ccf0e6d88dff776680d715817bbbc00
Gilberto <gil79430@gmail.com>
2026-08-14 07:29:17 -0300

    feat(why-choose-us): Reorganiza os cards da seção 'Why Choose Us' em duas colunas de dois cards

--- app/components/sections/Reasons.tsx
+++ app/components/sections/Reasons.tsx
@@ -33,33 +33,32 @@ export default function Reasons() {
     ];
 
     return(
-        <section className="flex items-center gap-5 px-5 overflow-scroll w-full h-fit no-scrollbar">
-            {
-                
-                whyChooseUsFeatures.map((features, index) => {
-                    const title = features.title;
-                    const description = features.description;
-                    const icon = features.iconName;
-
-                    return (
-                        <motion.div className="flex flex-col items-center py-5"
-                            key={index}
-                            initial={{opacity: 0}}
-                            whileInView={{opacity: 1}}
-                            viewport={{once: true, margin: "-15px"}}
-                            transition={{duration: 0.4, delay: 0.3}}>
-
-                            <Cards className="flex flex-col p-5 gap-5 text-shadow-lg w-[80vw] h-80 md:w-[45vw]">
-                                <div className="flex justify-between items-center">
-                                    <img className="w-20" src={`${icon}.svg`} alt="sparkles icon"/>
-                                    <h3 className="flex-1 text-center flex flex-col justify-center text-2xl h-20"><strong>{title}</strong></h3>
-                                </div>
-                                <p className="text-lg">{description}</p>
-                            </Cards>
-                        </motion.div>
-                    );
-                })
-            }
+        <section className="flex items-center gap-5 overflow-scroll w-full h-fit no-scrollbar justify-center lg:px-0">
+            <div className="flex gap-5 overflow-scroll px-5 no-scrollbar lg:grid lg:grid-cols-2 lg:gap-5 lg:py-5">
+                {
+                    whyChooseUsFeatures.map((features, index) => {
+                        const title = features.title;
+                        const description = features.description;
+                        const icon = features.iconName;
+                        return (
+                            <motion.div className="flex flex-col items-center py-5 lg:py-0 lg:w-fit lg:h-fit"
+                                key={index}
+                                initial={{opacity: 0}}
+                                whileInView={{opacity: 1}}
+                                viewport={{once: true, margin: "-15px"}}
+                                transition={{duration: 0.4, delay: 0.3}}>
+                                <Cards className="flex flex-col p-5 gap-5 text-shadow-lg w-[80vw] h-80 md:w-[45vw] lg:w-[45vw]">
+                                    <div className="flex justify-between items-center">
+                                        <img className="w-20" src={`${icon}.svg`} alt="sparkles icon"/>
+                                        <h3 className="flex-1 text-center flex flex-col justify-center text-2xl h-20"><strong>{title}</strong></h3>
+                                    </div>
+                                    <p className="text-lg">{description}</p>
+                                </Cards>
+                            </motion.div>
+                        );
+                    })
+                }
+            </div>
         </section>
     );
 }
