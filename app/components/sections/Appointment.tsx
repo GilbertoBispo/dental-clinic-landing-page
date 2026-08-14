51cf1a1fd9103aeae7dc162ca4ea2841e7ec0063
Gilberto <gil79430@gmail.com>
2026-08-14 16:50:40 -0300

    feat(appointment): Alinha os dois botões do título da seção horizontalmente

--- app/components/sections/Appointment.tsx
+++ app/components/sections/Appointment.tsx
@@ -8,8 +8,10 @@ export default function Appointment() {
                 <h3 className="font-bold text-center">Ready for a Healthier, Brighter Smile?</h3>
                 <p className="text-center">Book your consultation today and take the first step toward the gentle, top-tier dental care you deserve. We're here to help you smile with confidence.</p>
 
-                <Button className="">Schedule Your Visit</Button>
-                <Button className="bg-[#25D366] flex justify-between"><img className="w-6 h-6 mr-2" src="/whatsapp.svg" alt="whatsapp icon" /> Chat on WhatsApp</Button>
+                <div className="flex flex-col items-center gap-5 lg:flex-row">
+                    <Button className="">Schedule Your Visit</Button>
+                    <Button className="bg-[#25D366] flex justify-between"><img className="w-6 h-6 mr-2" src="/whatsapp.svg" alt="whatsapp icon" /> Chat on WhatsApp</Button>
+                </div>
             </div>
         </section>
     );
